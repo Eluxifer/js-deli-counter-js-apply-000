3895f789ff20246176a77af45c982b1802478e85
Jeison Rodriguez <jeisonrodriguez94@outlook.com>
2018-11-27 20:48:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ function nowServing(katzDeliLine){
   if (katzDeliLine.length === 0)
   {
     return "There is nobody waiting to be served!";
-    
+
   }
   else {
     return (`Currently serving ${katzDeliLine.shift()}.`)
   }
-  
+
 }

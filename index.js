15f2dae9576498e551d1a949505dae523ff85cff
Jeison Rodriguez <jeisonrodriguez94@outlook.com>
2018-11-27 20:48:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,19 @@ function takeANumber(people, name){
   people.push(`${name}`);
   return(`Welcome, ${name}. You are number ${people.length} in line.`);
 }
+
+function nowServing(katzDeliLine){
+  let i = 0;
+  while (i < katzDeliLine.length) {
+    i++;
+  }
+  if (katzDeliLine.length === 0)
+  {
+    return "There is nobody waiting to be served!";
+    
+  }
+  else {
+    return (`Currently serving ${katzDeliLine.shift()}.`)
+  }
+  
+}
